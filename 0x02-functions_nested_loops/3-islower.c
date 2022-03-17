3602e5e81739a4c25e1671efce7991bf4c4a9300
Emynuel <emynuelintelligent@gmail.com>
2022-03-16 20:02:50 -0700

    redo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,20 +3,17 @@
 /**
  * _islower - Short description, single line
  * @c: contains value to be compared
- * Return: Always 0
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ *
  */
 
 int _islower(int c)
 
 {
-	if (c < 'a' && c > 'z')
-	{
-		return (1);
-	}
+	if (c >= 'a' && c <= 'z')
 
+		return (1);
 	else
-	{
 		return (0);
-	}
 
 }
