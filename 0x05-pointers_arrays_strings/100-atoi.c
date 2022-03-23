adde9d456ef44bdfad8bcefaac6104e2faffe274
Emynuel <emynuelintelligent@gmail.com>
2022-03-22 21:53:27 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,6 +8,30 @@
 
 int _atoi(char *s)
 {
+	unsigned int number, i;
+	int sign;
 
-	return (0);
+	sign = 1;
+	number = 0;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_numerical(s[i]))
+		{
+			number = ((s[i] - 48) + number * 10);
+
+		if (s[i + 1] == ' ')
+		break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+
+	}
+
+
+
+	return (number * sign);
 }
